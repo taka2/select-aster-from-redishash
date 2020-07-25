dd5b69466f5ba38b5bd4d3bc5238ab34f414846c
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2020-07-25 14:32:06 +0900

    Close #4

--- src/main/java/select/aster/from/redishash/service/QueryParser.java
+++ src/main/java/select/aster/from/redishash/service/QueryParser.java
@@ -1,30 +1,38 @@
 package select.aster.from.redishash.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class QueryParser {
 	String regexp1 = "from (\\p{Alpha}+)";
 	String regexp2 = "from (\\p{Alpha}+) where (\\p{Alnum}+)=(\\p{Graph}+)";
+	String[] regexpArray = {regexp1, regexp2};
 
-	Pattern pattern1;
-	Pattern pattern2;
+	List<Pattern> patterns;
+	
 	public QueryParser() {
-		this.pattern1 = Pattern.compile(regexp1);
-		this.pattern2 = Pattern.compile(regexp2);
+		patterns = new ArrayList<>();
+		for(String regexp : regexpArray) {
+			patterns.add(Pattern.compile(regexp));
+		}
 	}
+	
 	public QueryData parseQuery(String query) {
-		Matcher matcher1 = pattern1.matcher(query);
-		if(matcher1.matches()) {
-			QueryData result = new QueryData(matcher1.group(1), null, null);
-			return result;
-		} else {
-			Matcher matcher2 = pattern2.matcher(query);
-			if(matcher2.matches()) {
-				QueryData result = new QueryData(matcher2.group(1), matcher2.group(2), matcher2.group(3));
-				return result;
+		for(Pattern pattern : patterns) {
+			Matcher matcher = pattern.matcher(query);
+			if(matcher.matches()) {
+				if(matcher.groupCount() == 1) {
+					QueryData result = new QueryData(matcher.group(1), null, null);
+					return result;
+				} else if(matcher.groupCount() == 3) {
+					QueryData result = new QueryData(matcher.group(1), matcher.group(2), matcher.group(3));
+					return result;
+				}
 			}
 		}
+		
 		return null;
 	}
 }
